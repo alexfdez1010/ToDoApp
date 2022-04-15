b2d97248507235a10864ebf386badbd118ff1b7d
Alejandro Fernández Camello <56976110+alexfdez1010@users.noreply.github.com>
2022-04-15 11:31:04 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <RandomQuotes :title="titleQuotes" />
+    <ToDoList :title="titleToDo"/>
+  </div>
+</template>
+
+<script>
+
+import ToDoList from './components/ToDoList.vue';
+import RandomQuotes from './components/RandomQuotes.vue';
+
+export default {
+  data() {
+    return {
+      titleToDo: 'To Do List',
+      titleQuotes: 'Famous Quotes'
+    }
+  },
+  name: 'App',
+  components: {
+    ToDoList,
+    RandomQuotes
+  }
+}
+
+</script>
+
+<style>
+
+</style>
